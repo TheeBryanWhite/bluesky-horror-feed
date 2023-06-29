aef0974e7e9d5ab3810a0771481aa181bf6ecbd6
Bryan White <arkham@Bryans-MacBook-Pro.local>
2023-06-29 09:48:07 -0400

    Updated publish script with feed specifics

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -8,28 +8,28 @@ const run = async () => {
 
   // YOUR bluesky handle
   // Ex: user.bsky.social
-  const handle = ''
+  const handle = 'grimbrospod.bsky.social'
 
   // YOUR bluesky password, or preferably an App Password (found in your client settings)
   // Ex: abcd-1234-efgh-5678
-  const password = ''
+  const password = 'ni2n-6kgh-iy36-c5h6'
 
   // A short name for the record that will show in urls
   // Lowercase with no spaces.
   // Ex: whats-hot
-  const recordName = ''
+  const recordName = 'bloody-feed-of-horror'
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = ''
+  const displayName = 'The Bloody Feed of Horror'
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
-  const description = ''
+  const description = 'A feed for fans of all things horror! . \n To publish to it use the knife emoji 🔪 \n Remember to ❤️ this feed and tell your fellow weirdos about it!'
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
-  const avatar: string = ''
+  const avatar: string = '~/images/jason-mask.png'
 
   // -------------------------------------
   // NO NEED TO TOUCH ANYTHING BELOW HERE
